f25cbba0beb1be6d3ff4b97bada5467ca83099fd
hongwei.lian <hongwei.lian@qdfae.com>
2017-11-22 20:02:32 +0800

    JDK集合新特性学习

--- JDK/src/test/java/com/qdfae/jdk/test/EnumTest.java
+++ JDK/src/test/java/com/qdfae/jdk/test/EnumTest.java
@@ -1,5 +1,14 @@
 package com.qdfae.jdk.test;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;import java.util.TooManyListenersException;
+import java.util.jar.Attributes.Name;
+import java.util.stream.Collectors;
+
 import org.junit.Test;
 
 public class EnumTest {
@@ -28,5 +37,77 @@ public class EnumTest {
 	public void testEnum(){
 		//EnumTest.operateType = 
 	}
+	
+	@Test
+	public void testMap(){
+		List<String> list1 = new ArrayList<>();
+		list1.add("Tom");
+		list1.add("Amy");
+		list1.add("Jack");
+		list1.add(null);
+		List<String> list2 = new ArrayList<>();
+		list2.add("Tom");
+		list2.add("Amy");
+		list2.add("Jack");
+		List<String> list3 = new ArrayList<>();
+		list3.add("Tom");
+		list3.add("Amy");
+		list3.add("Jack");
+		list3.add(null);
+		Map<Integer, List<String>> map = new HashMap<>();
+		map.put(1, list1);
+		map.put(2, list2);
+		map.put(3, list3);
+		
+//		map.forEach((key, value) -> {
+//			value.stream()
+//			         .filter(name -> Objects.nonNull(name))
+//			         .forEach(name -> {
+//			        	 if (name.equals("Jack")) {
+//			        		 System.out.println(name);
+//			        	 }
+//			         });
+//		});
+		
+//		map.values().forEach(list -> {
+//			list.stream()
+//	            .filter(name -> Objects.nonNull(name))
+//		        .forEach(name -> {
+//		        	 if (name.equals("Jack")) {
+//		        		 System.out.println(name);
+//		        	 }
+//		         });
+//		});
+		
+		map.values().stream()
+		                    .flatMap(toList -> toList.stream().filter(name -> Objects.nonNull(name)))
+		                    .forEach(name -> {
+		                    	if (name.equals("Jack")) {
+		                    		System.out.println(name);
+		   		        	    }
+		                    });
+		
+		
+		
+		
+	}
+	
+	
+	@Test
+	public void testList(){
+		List<String> list = new ArrayList<>();
+		list.add("Tom");
+		list.add("Amy");
+		list.add("Jack");
+		
+		list.stream()
+		    .filter(name -> name.equals("m"))
+		    .filter(name -> name.equals("a"))
+		    .forEach(name -> System.out.println(name));
+		
+		
+	}
+		
+	
 
 }
