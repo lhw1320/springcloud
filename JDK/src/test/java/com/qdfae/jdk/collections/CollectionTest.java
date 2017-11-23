5a6a9ed0d930d610ce05af0a283e1275b63bf393
lhw1320 <lhw1320@126.com>
2017-11-23 23:15:58 +0800

    JDK8新特性和doc文档建立

---
+++ JDK/src/test/java/com/qdfae/jdk/collections/CollectionTest.java
@@ -0,0 +1,16 @@
+package com.qdfae.jdk.collections;
+
+public class CollectionTest {
+	
+	/**
+	 *  
+	 * @author hongwei.lian  
+	 * @date 2017年11月23日 下午10:30:44
+	 */
+	public void testList1(){
+		
+	}
+	
+	
+
+}
