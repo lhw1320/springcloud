6454ab143c4d1be4cae54cfe976d6ed80d2813eb
hongwei.lian <hongwei.lian@qdfae.com>
2017-12-19 19:25:05 +0800

    添加

--- JDK/src/test/java/com/qdfae/jdk/test/MapTest.java
+++ JDK/src/test/java/com/qdfae/jdk/test/MapTest.java
@@ -1,7 +1,9 @@
 package com.qdfae.jdk.test;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.junit.Test;
@@ -22,6 +24,26 @@ public class MapTest {
 	public void test1(){
 		Integer id = 1;
 		System.out.println(1 == id);
+	}
+	
+	@Test
+	public void test2(){
+		List<Integer> list1 = new ArrayList<>();
+		list1.add(1);
+		list1.add(2);
+		list1.add(3);
+		list1.add(4);
+		List<Integer> list2 = new ArrayList<>();
+		list2.add(1);
+		list2.add(2);
+		list1.removeAll(list2);
+		list1.forEach(System.out::println);
+	}
+	
+	@Test
+	public void test3(){
+		Integer id = 1;
+		System.out.println(id == 1);
 	}
 
 }
