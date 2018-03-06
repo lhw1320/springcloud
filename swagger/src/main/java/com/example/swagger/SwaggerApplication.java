18d1abacffbdf58b5021d28e674ef31e0bf17899
Administrator <Administrator@lianhongwei>
2018-03-06 20:02:43 +0800

    添加swagger依赖

---
+++ swagger/src/main/java/com/example/swagger/SwaggerApplication.java
@@ -0,0 +1,19 @@
+package com.example.swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ *
+ * @author hongwei.lian
+ * @date 2018年3月6日 下午8:01:23
+ */
+@SpringBootApplication
+public class SwaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerApplication.class, args);
+	}
+	
+}
