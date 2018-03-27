06a43e589acb9c75f177fb0b5202c1862806e911
Administrator <Administrator@lianhongwei>
2018-03-27 19:02:20 +0800

    新建

---
+++ mqtest/src/main/java/com/example/demo/MqtestApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MqtestApplication.class, args);
+	}
+}
