cbb0c3ab4103368f9864cef00ff767e7c1cdb09c
lhw1320 <lhw1320@126.com>
2018-04-01 20:56:32 +0800

    更新

--- JDK/src/test/java/com/qdfae/jdk/collection/RemoveListTest.java
+++ JDK/src/test/java/com/qdfae/jdk/collection/RemoveListTest.java
@@ -3,6 +3,7 @@ package com.qdfae.jdk.collection;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 import org.junit.Before;
@@ -15,13 +16,29 @@ import com.qdfae.jdk.domain.SystypeBaseVo;
  * 可能产生问题：
  * 1、删除元素后List的元素数量会发生变化，随之索引也会发生变化
  * 2、对List进行删除操作可能会产生并发问题，遍历List的时候不允许并发操作
+ * 
+ * 本次需求：
+ * 从已有的List列表中移除typeId是3和4的对象
  *
  * @author hongwei.lian
  * @date 2018年3月28日 下午3:37:30
  */
 public class RemoveListTest {
 	
+	/**
+	 * 存储对象的列表
+	 */
 	private List<SystypeBaseVo> list;
+	
+	/**
+	 * 存储去除元素列表
+	 */
+	private List<SystypeBaseVo> removeList;
+	
+	/**
+	 * 存储去除后的元素列表
+	 */
+	private List<SystypeBaseVo> removedList;
 	
 	/**
 	 * 初始化列表
@@ -35,30 +52,72 @@ public class RemoveListTest {
 		list.add(new SystypeBaseVo(1, 8, 1, "次"));
 		list.add(new SystypeBaseVo(2, 8, 2, "年化"));
 		list.add(new SystypeBaseVo(3, 8, 3, "每期"));
+		list.add(new SystypeBaseVo(4, 8, 4, "其它"));
 	}
 	
+	/**
+	 * 使用普通for循环遍历，没有抛出异常，但结果与预期不符合
+	 * 使用ArrayList实现List接口的public E remove(int index)方法
+	 *  
+	 * @author hongwei.lian  
+	 * @date 2018年4月1日 下午6:15:48
+	 */
 	@Test
-	public void testListRemove11() {
+	public void testListRemove1() {
 		for (int i = 0; i < list.size(); i++) {
-			if (list.get(i).getTypeId() == 3) {
+			//-- 等价于list.get(i).getTypeId() == 3 || list.get(i).getTypeId() == 4
+			if (list.get(i).getTypeId() >= 3) {
 				list.remove(i);
+			}
+		}
+		list.forEach(System.out::println);
+	}
+
+	/**
+	 * 使用普通for循环遍历，没有抛出异常，但结果与预期不符合
+	 * 使用ArrayList实现List接口的public boolean remove(Object o)方法
+	 *  
+	 * @author hongwei.lian  
+	 * @date 2018年4月1日 下午7:15:03
+	 */
+	@Test
+	public void testListRemove12() {
+		for (int i = 0; i < list.size(); i++) {
+			SystypeBaseVo vo = list.get(i);
+			//-- 等价于vo.getTypeId() == 3 || vo.getTypeId() == 4
+			if (vo.getTypeId() >= 3) {
+				list.remove(vo);
 			}
 		}
 		list.forEach(System.out::println);
 	}
 	
 	/**
-	 * 
-	 *  抛出ConcurrentModificationException异常
+	 * 使用增强for循环遍历，与上述结果一致，依然与预期结果不符合
 	 *  
-	 *
+	 * @author hongwei.lian  
+	 * @date 2018年4月1日 下午7:56:54
+	 */
+	@Test
+	public void testListRemove2() {
+		for (SystypeBaseVo vo : list) {
+			if(vo.getTypeId() >= 3) {
+				list.remove(vo);
+			}
+		}
+		list.forEach(System.out::println);
+	}
+	
+	/**
+	 *  使用JDK8提供的增强for循环遍历，直接抛出ConcurrentModificationException异常
+	 *  
 	 * @author hongwei.lian
 	 * @date 2018年3月28日 下午2:30:42
 	 */
 	@Test
-	public void testListRemove12() {
+	public void testListRemove21() {
 		list.forEach(vo -> {
-			if(vo.getTypeId() == 3) {
+			if(vo.getTypeId() >= 3) {
 				list.remove(vo);
 			}
 		});
@@ -66,7 +125,7 @@ public class RemoveListTest {
 	}
 	
 	/**
-	 * 
+	 * 使用迭代器遍历
 	 * ArrayList类中的内部类Itr重写了Iterator接口中的remove()方法
 	 * 因此使用迭代器遍历列表中的元素的同时可以移除其中的元素，不会抛出异常
 	 *
@@ -74,75 +133,94 @@ public class RemoveListTest {
 	 * @date 2018年3月28日 下午2:30:48
 	 */
 	@Test
-	public void testListRemove13() {
+	public void testListRemove3() {
 		Iterator<SystypeBaseVo> it = list.iterator();
 		while(it.hasNext()) {
-			if(it.next().getTypeId() == 3)
+			if(it.next().getTypeId() >= 3)
 				it.remove();
 		}
 		list.forEach(System.out::println);
 	}
 	
 	/**
-	 * 
-	 * 使用JDK8的Stream接口的filter()方法
-	 *
-	 * @author hongwei.lian
-	 * @date 2018年3月28日 下午2:33:55
-	 */
-	@Test
-	public void testListRemove3() {
-		List<SystypeBaseVo> removeList = list.stream()
-				.filter(vo -> vo.getTypeId() != 3)
-				.collect(Collectors.toList());
-		removeList.forEach(System.out::println);
-	}
-	
-	/**
-	 * 
+	 * 使用List接口的removeAll()方法
 	 *
 	 * @author hongwei.lian
 	 * @date 2018年3月28日 下午3:22:53
 	 */
 	@Test
 	public void testListRemove4() {
-		//-- 存储需要删除的元素
-		List<SystypeBaseVo> removeList = new ArrayList<>();
+		removeList = new ArrayList<>();
 		list.forEach(vo -> {
-			if(vo.getTypeId() == 3) {
+			if(vo.getTypeId() >= 3) {
 				removeList.add(vo);
 			}
 		});
-		//-- 从原先列表中移除
 		list.removeAll(removeList);
 		list.forEach(System.out::println);
 	}
 	
 	/**
-	 * 方案：
+	 * 使用JDK8提供的Predicate判断接口
+	 *  or()方法和negate()方法和test()方法
+	 *  
+	 * @author hongwei.lian  
+	 * @date 2018年4月1日 下午6:31:20
+	 */
+	@Test
+	public void testListRemove5() {
+		Predicate<SystypeBaseVo> filter = vo -> vo.getTypeId() == 3;
+		removedList = list.stream()
+				.filter(filter
+						.or(vo -> vo.getTypeId() == 4)
+						.negate())
+				.collect(Collectors.toList());
+		removedList.forEach(System.out::println);
+	}
+	
+	/**
+	 * 使用JDK8提供的Predicate判断接口
+	 *  and()方法和test()方法
+	 *  
+	 * @author hongwei.lian  
+	 * @date 2018年4月1日 下午8:22:07
+	 */
+	@Test
+	public void testListRemove51() {
+		Predicate<SystypeBaseVo> filter = vo -> vo.getTypeId() != 3;
+		removedList = list.stream()
+				.filter(filter
+						.and(vo -> vo.getTypeId() != 4))
+				.collect(Collectors.toList());
+		removedList.forEach(System.out::println);
+	}
+	
+	/**
+	 * 使用JDK8提供的Predicate判断接口
+	 * test()方法
+	 *  
+	 * @author hongwei.lian  
+	 * @date 2018年4月1日 下午8:22:40
+	 */
+	@Test
+	public void testListRemove52() {
+		removedList = list.stream()
+				.filter(vo -> vo.getTypeId() < 3)
+				.collect(Collectors.toList());
+		removedList.forEach(System.out::println);
+	}
+	
+	/**
 	 * 使用JDK8提供的Collection接口中的removeIf()方法
-	 * 源码：
-	 *  default boolean removeIf(Predicate<? super E> filter) {
-	 *      Objects.requireNonNull(filter);
-	 *      boolean removed = false;
-	 *      final Iterator<E> each = iterator();
-	 *      while (each.hasNext()) {
-	 *           if (filter.test(each.next())) {
-	 *               each.remove();
-	 *               removed = true;
-	 *          }
-	 *       }
-	 *       return removed;
-	 *  }
-	 * 可以看出使用的是迭代器遍历移除的方式，只是做了简单封装
+	 * 看源码可知使用的是迭代器遍历移除的方式，只是做了简单封装
 	 *
 	 * @author hongwei.lian
 	 * @date 2018年3月28日 下午3:24:57
 	 */
 	@Test
-	public void testListRemove5() {
+	public void testListRemove6() {
 		//-- 符合条件的从列表中移除
-		list.removeIf(vo -> vo.getTypeId() == 3);
+		list.removeIf(vo -> vo.getTypeId() >= 3);
 		list.forEach(System.out::println);
 	}
 	
