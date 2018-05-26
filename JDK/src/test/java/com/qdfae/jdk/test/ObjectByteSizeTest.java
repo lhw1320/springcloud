21c80c22c015af60e18f3bc94c3ba163c9a2910c
lhw1320 <lhw1320@126.com>
2018-05-26 17:58:43 +0800

    获取对象占用内存大小方法

---
+++ JDK/src/test/java/com/qdfae/jdk/test/ObjectByteSizeTest.java
@@ -0,0 +1,25 @@
+package com.qdfae.jdk.test;
+
+import org.junit.Test;
+
+/**
+ * 数据类型占用内存大小测试
+ * 
+ * @author hongwei.lian 
+ * @date 2018年5月26日 下午5:33:48
+ */
+public class ObjectByteSizeTest {
+	
+	/**
+	 * 获取一个对象占用内存空间的字节大小
+	 * 1byte = 8bit
+	 *  
+	 * @author hongwei.lian  
+	 * @date 2018年5月26日 下午5:37:07
+	 */
+	@Test
+	public void testGetObjectByteSize1() {
+		
+	}
+
+}
