20d023b80d336c1b4cef97b27d6e9157a1a75233
lhw1320 <lhw1320@126.com>
2018-06-24 20:34:18 +0800

    添加velocity

--- spring/src/main/java/com/qdfae/spring/Application.java
+++ spring/src/main/java/com/qdfae/spring/Application.java
@@ -1,9 +1,14 @@
 package com.qdfae.spring;
 
+import java.io.IOException;
+import java.util.Properties;
+
+import org.apache.velocity.app.VelocityEngine;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 
 /**
  * 启动类
@@ -21,6 +26,21 @@ public class Application extends SpringBootServletInitializer {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+	
+	/**
+	 * 注入VelocityEngine
+	 * 
+	 * @return
+	 * @throws IOException 
+	 * @author hongwei.lian  
+	 * @date 2018年6月24日 下午6:40:50
+	 */
+	@Bean
+	VelocityEngine velocityEngine() throws IOException {
+	    Properties properties = new Properties();
+		properties.load(getClass().getResourceAsStream("/velocity.properties"));
+	    return new VelocityEngine(properties);
 	}
 	
 }
